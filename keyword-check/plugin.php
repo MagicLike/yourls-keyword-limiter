0bd777f50016b911faee60f5d81dffb054069446
Reza <za22061991@gmail.com>
2024-02-21 11:46:06 +0700

    add regex pattern modifier in admin page

--- keyword-check/plugin.php
+++ keyword-check/plugin.php
@@ -36,6 +36,8 @@ function keyword_check_do_page() {
 
 	// Get value from database
 	$max_length = yourls_get_option( 'max_length_opt' );
+	$pattern = yourls_get_option( 'pattern_opt' );
+	$pattern_enable = yourls_get_option( 'pattern_enable' );
 
 	// Create nonce
 	$nonce = yourls_create_nonce( 'kwcheck_page' );
@@ -45,10 +47,30 @@ function keyword_check_do_page() {
 		<form method="post">
 		<input type="hidden" name="nonce" value="$nonce" />
 		<p><label for="max_length_opt">Maximum keyword length</label> <input type="text" id="max_length_opt" name="max_length_opt" value="$max_length" /></p>
-		<p><input type="submit" value="Update value" /></p>
+		<p><input type="checkbox" id="pattern_enable" name="pattern_enable" onclick="document.getElementById('pattern_opt').disabled=!this.checked;" value="enable">
+			<label for="pattern_opt">Enable RegEx pattern limiter</label>
+			<input type="text" id="pattern_opt" name="pattern_opt" value="$pattern" /></p>
+		<p><input type="submit" value="Update value"/></p>
 		</form>
 
 HTML;
+
+	if( $pattern_enable ) {
+	echo <<<HTML
+		<script>
+			document.getElementById("pattern_enable").checked = true;
+			document.getElementById("pattern_opt").disabled = false;
+		</script>
+HTML;
+	}
+	else {
+	echo <<<HTML
+		<script>
+			document.getElementById("pattern_enable").checked = false;
+			document.getElementById("pattern_opt").disabled = true;
+		</script>
+HTML;
+	}
 }
 
 //Update option
@@ -59,6 +81,15 @@ function keyword_check_update_option() {
 		$max_length_in = intval($max_length_in);
 		yourls_update_option( 'max_length_opt', $max_length_in );
 	}
+
+	if( isset( $_POST['pattern_enable'] ) && $_POST['pattern_enable'] == "enable") {
+		$regex = strval($_POST['pattern_opt']);
+		yourls_update_option( 'pattern_opt', $regex );
+		yourls_update_option( 'pattern_enable', TRUE );
+	}
+	else {
+		yourls_update_option( 'pattern_enable', FALSE);
+	}
 }
 
 // Hook our custom function into the 'shunt_add_new_link' filter
@@ -67,7 +98,8 @@ yourls_add_filter( 'shunt_add_new_link', 'keyword_check' );
 // Check the keyword
 function keyword_check( $too_long, $url, $keyword ) {
 	$max_length = yourls_get_option( 'max_length_opt' );
-	$pattern = "/(?<!\d)\d{5,6}(?!\d)/";
+	$pattern_enable = yourls_get_option( 'pattern_enable' );
+	$pattern = yourls_get_option( 'pattern_opt' );
         $kwlist = array('theft', 'fridge', 'porn', 'accesscontrol', 'rfid', 'refrigerator',
                         'growkit', 'freeze', 'lossprevention', 'gacor', 'retail', 'sex',
                         'ledh', 'smartshelf', 'goldenteacher', 'mattress', 'ledh', 'ledp',
@@ -79,10 +111,10 @@ function keyword_check( $too_long, $url, $keyword ) {
 		$return['message']  = "Sorry, the keyword is too long. It can't be more than " . $max_length . " characters.";
 		return yourls_apply_filter( 'add_new_link_keyword_too_long', $return );
 	}
-	elseif ( preg_match($pattern, $keyword ) > 0 ) {
+	elseif ( $pattern_enable && preg_match( $pattern, $keyword ) > 0 ) {
 		$return['status']   = 'fail';
 		$return['code']     = 'error:keyword';
-		$return['message']  = "Sorry, the keyword is forbidden.";
+		$return['message']  = "Sorry, the keyword pattern is forbidden.";
 		return yourls_apply_filter( 'add_new_link_number', $return );
 	}
 	else {
